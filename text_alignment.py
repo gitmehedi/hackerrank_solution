577711ae0cb21195981efe248118f243558b5e3a
Mehedi Hasan <md.mehedi.info@gmail.com>
2016-03-17 12:31:43 +0600

    [ADD] Change on text-alignment

--- text_alignment.py
+++ text_alignment.py
@@ -9,18 +9,44 @@ for i in range(textInput):
 	
 for i in range(textInput):
 	s = textInput*text
-	print s.center(width,' '),s.center(width,' ')
+	print s.center(textInput*2)+s.center(textInput*6)
 	
 for i in range(textInput):
-	s = textInput*(textInput-1)*text+text 
-	print s.center(width*3,' ')
+	s = textInput*textInput*text 
+	print s.center(textInput*6)
 
 for i in range(textInput):
 	s = textInput*text
-	print s.center(width,' '),s.center(width,' ')
+	print s.center(textInput*2)+s.center(textInput*6)
 
 for i in range(textInput-1,-1,-1):
 	s = (i*2)*text+text
-	print ''.center(width*2,' '),s.center(width,' ')
+	print ''.center(textInput*4-1,' '),s.center(width,' ')
+
+
+"""
+thickness = int(raw_input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print (c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1)
+
+#Top Pillars
+for i in range(thickness+1):
+    print (c*thickness).center(thickness*2)+(c*thickness).center(thickness*6)
+
+#Middle Belt
+for i in range((thickness+1)/2):
+    print (c*thickness*5).center(thickness*6)    
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print (c*thickness).center(thickness*2)+(c*thickness).center(thickness*6)    
+
+#Bottom Cone
+for i in range(thickness):
+    print ((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6) 
+"""
 
 	
